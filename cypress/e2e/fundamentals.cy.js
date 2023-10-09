f14dc8b1273203e02676be958c83962cd58393de
bla-risma <bla@rismasystems.com>
2023-10-09 16:21:16 +0200

    update

---
+++ cypress/e2e/fundamentals.cy.js
@@ -0,0 +1,19 @@
+describe('Fundamentals test', () => {
+  beforeEach(() => {
+    cy.visit('/fundamentals')
+  })
+  it('Contains correct header text', () => {
+    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+  })
+  it('Accordion works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i)
+      .should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]')
+      .click()
+    cy.contains(/Your tests will exist in a describe block/i)
+      .should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]')
+      .click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+  })
+})
